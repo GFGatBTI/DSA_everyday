7292bd8fad84ae91bde1b6e70c4c1b278fb65acf
GFGatBTI <97793374+GFGatBTI@users.noreply.github.com>
2022-01-15 20:39:13 +0530

    Update Left_view_of_a_binary_tree.cpp

--- 100_Days_Of_Code/Left_view_of_a_binary_tree.cpp
+++ 100_Days_Of_Code/Left_view_of_a_binary_tree.cpp
@@ -1,3 +1,21 @@
+/* Qn] Given a Binary Tree, print Left view of it. 
+Left view of a Binary Tree is set of nodes visible when tree is visited from Left side. 
+
+The task is to complete the function leftView(), which accepts root of the tree as argument.
+
+Topic: Queue
+Complete question Link: https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1 .
+
+References from our side:
+i] you must know the basics of the queue.
+ii] Implementation hint: https://www.geeksforgeeks.org/print-left-view-binary-tree/ .
+
+join us now, https://solo.to/gfgatbti to get displayed on our social media as geeks of the month, share the solution on LinkedIn and push your code to GitHub and tag us.*/ 
+
+// #gfgatbti #gfgatbti_01
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
