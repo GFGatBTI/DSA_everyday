5c618682a631aa1b4c005b4a9cbdce67e37fd515
GFGatBTI <97793374+GFGatBTI@users.noreply.github.com>
2022-01-19 21:08:07 +0530

    Add files via upload

---
+++ 100_Days_Of_Code/5. square  root of a number.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+  
+
+ // } Driver Code Ends
+//User function Template for C
+
+long long int floorSqrt(long long int x) 
+{
+    // Your code goes here   
+    int a = sqrt(x);
+       return a;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		long long n;
+		scanf("%ld", &n);
+	
+		printf("%ld\n", floorSqrt(n));
+	}
+    return 0;   
+}
+  // } Driver Code Ends
