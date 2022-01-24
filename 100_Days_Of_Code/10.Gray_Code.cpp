5c93e831d142e54f87cb535db9f24a8770e23a6e
GeeksForGeeks at BTI <97793374+GFGatBTI@users.noreply.github.com>
2022-01-24 22:51:38 +0530

    Update 10.Gray_Code.cpp

--- 100_Days_Of_Code/10.Gray_Code.cpp
+++ 100_Days_Of_Code/10.Gray_Code.cpp
@@ -1,3 +1,6 @@
+/*Gray Code  :
+Given a number N, generate bit patterns from 0 to 2^N-1 such that successive patterns differ by one bit. 
+A Gray code sequence must begin with 0.*/
 
 // { Driver Code Starts
 //Initial Template for C++
