8325483e6b3670585d999912a5461f9b6302b913
GeeksForGeeks at BTI <97793374+GFGatBTI@users.noreply.github.com>
2022-01-28 22:13:20 +0530

    Add files via upload

---
+++ 100_Days_Of_Code/14. Median in a stream.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+class Solution
+{
+   public:
+   //Function to insert heap.
+   priority_queue<int> maxH;
+   priority_queue<int,vector<int>,greater<int>> minH;
+   // int c=0;
+   
+   void insertHeap(int &x)
+   {
+       if(maxH.size()==0){
+           maxH.push(x);
+       }
+       else if(x<maxH.top()){
+           maxH.push(x);
+       }
+       else{
+           minH.push(x);
+       }
+       // c++;
+       balanceHeaps();
+   }
+   
+   //Function to balance heaps.
+   void balanceHeaps()
+   {
+       if(maxH.size()>minH.size() && (maxH.size()-minH.size() ) >1){
+           int t=maxH.top();
+           maxH.pop();
+           minH.push(t);
+       }
+       else if(maxH.size()<minH.size() && (minH.size()-maxH.size() ) >1){
+           int t=minH.top();
+           minH.pop();
+           maxH.push(t);
+       }
+   }
+   
+   //Function to return Median.
+   double getMedian()
+   {
+       if(maxH.size()==minH.size()){
+           double m=(maxH.top()+minH.top())/2.0;
+           return m;
+       }
+       else{
+           if(maxH.size()>minH.size()){
+               return maxH.top();
+           }
+           else{
+               return minH.top();
+           }
+       }
+   }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int n, x;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	Solution ob;
+    	cin >> n;
+    	for(int i = 1;i<= n; ++i)
+    	{
+    		cin >> x;
+    		ob.insertHeap(x);
+    	    cout << floor(ob.getMedian()) << endl;
+    	}
+    }
+	return 0;
+}  // } Driver Code Ends
